3c272fe188f0fe366670b6e07b2764c43600eb7e
chanderah <68021568+chanderah@users.noreply.github.com>
2023-07-10 17:25:20 +0700

    1 file modified

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,9 +4,9 @@ import { Validators } from '@angular/forms';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
   form: FormGroup;
